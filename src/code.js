54a970d9e5970a7280729be5b3f5a8c62c6d4dc6
RhysH <rhys.handley@santander.co.uk>
2022-04-26 01:31:24 +0100

    # ui.html - Implemented an unwarranted workaround for displaying it won't work on numbers - Changed some funcs to ES6 - A couple of styling changes
    # code.js
    - Mostly changes to ES6
    - Dodgy workaround for no numbers (remove ASAP)

--- src/code.js
+++ src/code.js
@@ -4,9 +4,9 @@ var sentenceWeight = 1.015;
 var wordWeight = 84.6;
 var base = 206.835;
 
-function flesch (counts) {
+const flesch = counts => {
   if (!counts || !counts.sentence || !counts.word || !counts.syllable) {
-    return Number.NaN || 0;
+    return Number.NaN || "I can't do numbers :(";
   }
 
   const total = Number(
@@ -19,11 +19,11 @@ function flesch (counts) {
   if (total > 100) return 100;
 
   return total;
-}
+};
 
-function grade (counts) {
+const grade = counts => {
   if (!counts || !counts.sentence || !counts.word || !counts.syllable) {
-    return Number.NaN || 0;
+    return Number.NaN || "I can't do numbers :(";
   }
 
   const total = Math.round(
@@ -36,90 +36,79 @@ function grade (counts) {
   if (total > 18) return 18;
 
   return total;
-}
+};
 
-function words (string) {
+const words = string => {
   return string.split(' ').length;
-}
+};
 
-function sentences (string) {
+const sentences = string => {
   return string.split('. ').length;
-}
+};
 
 figma.showUI(__html__);
 
-figma.ui.onmessage = msg => {
-  if (msg.type === 'check-text') {
-    var numberOfSyllables = syllable(msg.text);
-		// console.log(numberOfSyllables)
-    var numberOfWords = words(msg.text);
-		// console.log(numberOfWords)
-    var numberOfSentences = sentences(msg.text);
-		// console.log(numberOfSentences)
-    var readingScore = flesch({
-      word: numberOfWords,
-      syllable: numberOfSyllables,
-      sentence: numberOfSentences
-    });
-		// console.log(readingScore)
-    figma.ui.postMessage(readingScore);
-  }
-};
-
 const scoreToAgeObj = {
-  'Under 6': 0,
-  '6 - 7': 1,
-  '7 - 8': 2,
-  '8 - 9': 3,
-  '9 - 10': 4,
-  '10 - 11': 5,
-  '11 - 12': 6,
-  '12 - 13': 7,
-  '13 - 14': 8,
-  '14 - 15': 9,
-  '15 - 16': 10,
-  '16 - 17': 11,
-  '17 - 18': 12
+  0: 'Under 6',
+  1: '6 - 7',
+  2: '7 - 8',
+  3: '8 - 9',
+  4: '9 - 10',
+  5: '10 - 11',
+  6: '11 - 12',
+  7: '12 - 13',
+  8: '13 - 14',
+  9: '14 - 15',
+  10: '15 - 16',
+  11: '16 - 17',
+  12: '17 - 18'
 };
 
-const scoreToAge = e => {
-  for (const [key, value] of Object.entries(scoreToAgeObj)) {
-    if (e === value) return key;
-    if (e > 12) return 'Over 18';
-  }
+const scoreToAge = score => {
+  return score > 12
+		? 'Over 18'
+		: score === "I can't do numbers :(" ? score : scoreToAgeObj[score];
 };
 
-figma.ui.resize(300, 250);
+figma.ui.resize(300, 200);
 
 figma.on('selectionchange', e => {
   const selection = figma.currentPage.selection[0];
-  const selectexTextNode = figma.currentPage.selectedTextRange;
+
+	// const selectexTextNode = figma.currentPage.selectedTextRange;
 
 	// Enable this if you want to do it on highlighted text
 	//   let highlightedText = selection.characters.slice(
 	// 		selectexTextNode.start,
 	// 		selectexTextNode.end
 
+  if (selection && selection.type !== 'TEXT') {
+    const result = 'No Text';
+    const results = { score: result, grade: result };
+
+    figma.ui.postMessage(results);
+  }
+
   if (selection && selection.type === 'TEXT' && selection.characters.length) {
-    var numberOfSyllables = syllable(selection.characters);
+    const numberOfSyllables = syllable(selection.characters);
 		// console.log(numberOfSyllables);
-    var numberOfWords = words(selection.characters);
+    const numberOfWords = words(selection.characters);
 		// console.log(numberOfWords)
-    var numberOfSentences = sentences(selection.characters);
+    const numberOfSentences = sentences(selection.characters);
 		// console.log(numberOfSentences)
-    var readingScore = flesch({
+    const readingScore = flesch({
       word: numberOfWords,
       syllable: numberOfSyllables,
       sentence: numberOfSentences
     });
 
-    var readingGrade = grade({
+    const readingGrade = grade({
       word: numberOfWords,
       syllable: numberOfSyllables,
       sentence: numberOfSentences
     });
 
-    var readingAge = scoreToAge(readingGrade);
+    const readingAge = scoreToAge(readingGrade);
 
     const results = { score: readingScore, grade: readingAge };
 
