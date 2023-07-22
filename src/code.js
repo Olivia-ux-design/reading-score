5eb90063f00e8b0eb917a76e77f0ce0a6caf3f0e
RhysH <rhys.handley@santander.co.uk>
2023-07-22 17:47:59 +0100

    Score improvements
    - Score/accuracy is closer to Readable.com now
    - Updated the formula for splitting words and sentences
    - Some UI changes
    - Moved score reference to own file

--- src/code.js
+++ src/code.js
@@ -1,19 +1,22 @@
-import { syllable } from 'syllable';
+import { syllable } from "syllable";
+import { scoreToAgeObj } from "./grading";
 
 var sentenceWeight = 1.015;
 var wordWeight = 84.6;
 var base = 206.835;
 
-const flesch = counts => {
+const flesch = (counts) => {
   if (!counts || !counts.sentence || !counts.word || !counts.syllable) {
-    return Number.NaN || scoreToAgeObj['isNumber'].age;
+    return Number.NaN || scoreToAgeObj["isNumber"].age;
   }
 
   const total = Number(
-		(base -
-			sentenceWeight * (counts.word / counts.sentence) -
-			wordWeight * (counts.syllable / counts.word)).toPrecision(2)
-	);
+    (
+      base -
+      sentenceWeight * (counts.word / counts.sentence) -
+      wordWeight * (counts.syllable / counts.word)
+    ).toPrecision(2)
+  );
 
   if (total < 0) return 0;
   if (total > 100) return 100;
@@ -21,16 +24,16 @@ const flesch = counts => {
   return total;
 };
 
-const grade = counts => {
+const grade = (counts) => {
   if (!counts || !counts.sentence || !counts.word || !counts.syllable) {
     return Number.NaN;
   }
 
   const total = Math.round(
-		0.39 * (counts.word / counts.sentence) +
-			11.8 * (counts.syllable / counts.word) -
-			15.59
-	);
+    0.39 * (counts.word / counts.sentence) +
+      11.8 * (counts.syllable / counts.word) -
+      15.59
+  );
 
   if (total < 5) return 0;
   if (total > 18) return 18;
@@ -38,86 +41,42 @@ const grade = counts => {
   return total;
 };
 
-const words = string => {
-  return string.split(' ').length;
+const words = (string) => {
+  const getWords = string
+    .toLowerCase()
+    //   .replaceAll(/(^\w+'\w+)|,|\!|\?|\./g, '')
+    .replaceAll(/'\B|[^a-z' ]/g, "")
+    .split(" ")
+    .filter(Boolean);
+
+  return getWords.length;
 };
 
-const sentences = string => {
-  return string.split('. ').length;
+const sentences = (string) => {
+  const getPunctuation = string.match(/\!|\?|\./g);
+
+  return getPunctuation
+    ? string
+        .split(getPunctuation)
+        .map((x) => x.trim())
+        .filter(Boolean).length
+    : 1;
 };
 
-const scoreToAgeObj = {
-  0: {
-    grade: 'Under 5th grade/Year 6',
-    age: 'Under 10',
-    description: 'Very easy.'
-  },
-  5: {
-    age: '10 - 11',
-    description:
-			'Very easy to read. Easily understood by an average 11 year old school student.'
-  },
-  6: {
-    age: '11 - 12',
-    description: 'Easy to read. Conversational English for Consumers.'
-  },
-  7: {
-    age: '12 - 13',
-    description: 'Fairly easy to read.'
-  },
-  8: {
-    age: '13 - 14',
-    description:
-			'Plain English. Easily understood by 13 to 15 year old students.'
-  },
-  9: {
-    age: '14 - 15',
-    description:
-			'Plain English. Easily understood by 13 to 15 year old students.'
-  },
-  10: { age: '15 - 16', description: 'Fairly difficult to read.' },
-  11: { age: '16 - 17', description: 'Fairly difficult to read.' },
-  12: { age: '17 - 18', description: 'Fairly difficult to read.' },
-  13: { age: 'Over 18', description: 'Difficult to read.' },
-  14: { age: 'Over 18', description: 'Difficult to read.' },
-  15: {
-    age: 'Over 18',
-    description:
-			'Very difficult to read. Best understood by university graduates.'
-  },
-  16: {
-    age: 'Over 18',
-    description:
-			'Very difficult to read. Best understood by university graduates.'
-  },
-  17: {
-    age: 'Over 18',
-    description:
-			'Very difficult to read. Best understood by university graduates.'
-  },
-  18: {
-    age: 'Over 18',
-    description:
-			'Extremely difficult to read. Best understood by university graduates.'
-  },
-  isNumber: {
-    age: String.fromCodePoint(0x1f62c),
-    description: "I can't do numbers :("
-  }
-};
-
-const scoreToAge = score => {
+const scoreToAge = (score) => {
   return score < 5
-		? scoreToAgeObj[0]
-		: score > 18
-			? scoreToAgeObj[18]
-			: isNaN(score) ? scoreToAgeObj['isNumber'] : scoreToAgeObj[score];
+    ? scoreToAgeObj[0]
+    : score > 18
+    ? scoreToAgeObj[18]
+    : isNaN(score)
+    ? scoreToAgeObj["isNumber"]
+    : scoreToAgeObj[score];
 };
 
 figma.showUI(__html__, {
   width: 300,
   height: 250,
-  title: 'Flesch-Kincaid Score'
+  title: "Flesch-Kincaid Score"
 });
 
 // figma.on('run', async () => {
@@ -133,28 +92,29 @@ figma.showUI(__html__, {
 // };
 
 // Handles sending flesch results to the UI
-figma.on('selectionchange', e => {
+figma.on("selectionchange", (e) => {
   const selection = figma.currentPage.selection[0];
 
-  if (selection && selection.type !== 'TEXT') {
-    const result = 'No Text';
+  if (selection && selection.type !== "TEXT") {
+    const result = "No Text";
     const results = {
-      type: 'selection',
+      type: "selection",
       score: result,
       age: result,
-      description: ''
+      description: ""
     };
 
     figma.ui.postMessage(results);
   }
 
-  if (selection && selection.type === 'TEXT' && selection.characters.length) {
+  if (selection && selection.type === "TEXT" && selection.characters.length) {
     const numberOfSyllables = syllable(selection.characters);
-		// console.log(numberOfSyllables);
+    // console.log("# Syllables: ", numberOfSyllables);
     const numberOfWords = words(selection.characters);
-		// console.log(numberOfWords)
+    // console.log("# Words: ", numberOfWords);
     const numberOfSentences = sentences(selection.characters);
-		// console.log(numberOfSentences)
+    // console.log("# Sentences", numberOfSentences);
+
     const readingScore = flesch({
       word: numberOfWords,
       syllable: numberOfSyllables,
@@ -170,7 +130,7 @@ figma.on('selectionchange', e => {
     const readingAge = scoreToAge(readingGrade);
 
     const results = {
-      type: 'selection',
+      type: "selection",
       score: readingScore,
       age: readingAge.age,
       description: readingAge.description
---
+++ src/grading.js
@@ -0,0 +1,59 @@
+export const scoreToAgeObj = {
+  0: {
+    grade: "Under 5th grade/Year 6",
+    age: "Under 10",
+    description: "Very easy."
+  },
+  5: {
+    age: "10 - 11",
+    description:
+      "Very easy to read. Easily understood by an average 11 year old school student."
+  },
+  6: {
+    age: "11 - 12",
+    description: "Easy to read. Conversational English for Consumers."
+  },
+  7: {
+    age: "12 - 13",
+    description: "Fairly easy to read."
+  },
+  8: {
+    age: "13 - 14",
+    description:
+      "Plain English. Easily understood by 13 to 15 year old students."
+  },
+  9: {
+    age: "14 - 15",
+    description:
+      "Plain English. Easily understood by 13 to 15 year old students."
+  },
+  10: { age: "15 - 16", description: "Fairly difficult to read." },
+  11: { age: "16 - 17", description: "Fairly difficult to read." },
+  12: { age: "17 - 18", description: "Fairly difficult to read." },
+  13: { age: "Over 18", description: "Difficult to read." },
+  14: { age: "Over 18", description: "Difficult to read." },
+  15: {
+    age: "Over 18",
+    description:
+      "Very difficult to read. Best understood by university graduates."
+  },
+  16: {
+    age: "Over 18",
+    description:
+      "Very difficult to read. Best understood by university graduates."
+  },
+  17: {
+    age: "Over 18",
+    description:
+      "Very difficult to read. Best understood by university graduates."
+  },
+  18: {
+    age: "Over 18",
+    description:
+      "Extremely difficult to read. Best understood by university graduates."
+  },
+  isNumber: {
+    age: String.fromCodePoint(0x1f62c),
+    description: "I can't do numbers :("
+  }
+};
